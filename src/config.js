ec80d8e130bad4b33a9ae6e41733370577e4a96d
tmhammer <timh@govwizely.com>
2017-05-23 12:32:06 -0400

    First commit

---
+++ src/config.js
@@ -0,0 +1,22 @@
+import { assign } from './utils/lodash';
+
+const config = assign({
+  development: {
+    api: {
+      safe_harbor: {
+        host: 'https://api.trade.gov/v1/safe_harbor_swiss/search',
+        apiKey: 'O6fmOIPtrvDlqoDe2_6UbKJc',
+      },
+    },
+  },
+  production: {
+    api: {
+      safe_harbor: {
+        host: 'https://api.trade.gov/v1/safe_harbor_swiss/search',
+        apiKey: 'O6fmOIPtrvDlqoDe2_6UbKJc',
+      },
+    },
+  },
+});
+
+export default config[process.env.NODE_ENV];
